43839856c09e559a876b3ce1d1b7a30e9173301a
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-24 19:39:02 +0900

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by sakaguchi on 2024/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

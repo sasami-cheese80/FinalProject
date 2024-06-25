6a9c8d8a43412a22d4b16ce031b7c281e37007cb
kazuma_okada <kazuma_okada@mail.toyota.co.jp>
2024-06-25 13:41:21 +0900

    HOME画面作成

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            Home()
+            Home(groupData: GroupData())
                 .tabItem {
                     Image(systemName: "house")
                     Text("HOME")

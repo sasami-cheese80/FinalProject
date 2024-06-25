2f10a4cbb23f78a48f9e85fa3622e9c5ac6aaf67
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-25 11:47:56 +0900

    Ainori画面作成

--- FinalProject/Ainori.swift
+++ FinalProject/Ainori.swift
@@ -8,36 +8,82 @@
 import SwiftUI
 
 struct Ainori: View {
-    @State private var date = Date()
+//    @Binding var tabSelection: Int
+    @State var date: Date? = nil
+    @State var textValue:String = ""
+    @State var showDatePicker: Bool = false
     
     var body: some View {
-        VStack{
-            Text("利用日時")
-                .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
-                .fontWeight(.medium)
-                .padding(.leading)
-                .frame(maxWidth: .infinity, alignment: .leading)
 
+        ZStack{
+            VStack{
+                    
+                    Text("利用日時を選択")
+                        .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.medium)
+                        .padding(.leading, 40.0)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .disabled(true)
+                    
+                    
+                    
+                    
+                    HStack(alignment: .top){
+                        TextField("日時を選択", text: $textValue)
+                            .padding(.top,3)
+                        
+                        Button(action: {
+                            showDatePicker.toggle()
+                        }, label: {
+                            Image(systemName: "calendar")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.gray)
+                        })
+                        
+                    }
+                    .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                    .overlay(RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 1)
+                    )
+                    .padding(.init(top: 50, leading: 50, bottom: 100, trailing: 50))
+                    
+                    Button(action: {
+                        print("ここでpostします")
+//                        tabSelection = 3
+                    }, label: {
+                        Text("探す")
+                            .foregroundColor(.white)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding(.init(top: 15, leading: 70, bottom: 15, trailing: 70))
+                            .background(.black)
+                            .cornerRadius(5)
+                    })
+                    
+                }
+                //datepicker表示制御
+                if showDatePicker {
+                    CustomDatePicker(
+                        showDatePicker: $showDatePicker,
+                        savedDate: $date,
+                        sevedString: $textValue,
+                        selectedDate: date ?? Date()
+                    )
+                    .animation(.linear, value: date)
+                    .transition(.opacity)
+                    
+                    
+                }
                 
-            DatePicker("Dates", selection: $date)
-                .environment(\.locale, Locale(identifier:"ja_JP"))
-                .frame(width:337.0, height:300)
-                .datePickerStyle(.graphical)
-                .labelsHidden()
-                .background(.white)
-            
-            Button(action: {
-                print($date)
-            }, label: {
-                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-            })
-        }
-
+            }
+        
+        
     }
     
+
 }
 
 
 #Preview {
-    Ainori()
+    ContentView()
 }

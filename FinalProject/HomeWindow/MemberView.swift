9371da80c4e83f94e882cfad4a936cb4dc6ccb26
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-25 18:23:53 +0900

    home画面遷移追加

---
+++ FinalProject/HomeWindow/MemberView.swift
@@ -0,0 +1,18 @@
+//
+//  MenberView.swift
+//  FinalProject
+//
+//  Created by sakaguchi on 2024/06/25.
+//
+
+import SwiftUI
+
+struct MemberView: View {
+    var body: some View {
+        Text("Member表示画面")
+    }
+}
+
+#Preview {
+    MemberView()
+}
---
+++ FinalProject/HomeWindow/Plans.swift
@@ -0,0 +1,14 @@
+//
+//  PlansModel.swift
+//  FinalProject
+//
+//  Created by sakaguchi on 2024/06/25.
+//
+
+import Foundation
+
+struct Plans: Codable {
+    var id: Int
+    var date: String
+    var user_id: Int
+}

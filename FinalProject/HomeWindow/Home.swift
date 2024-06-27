86075e3c6265aede9633326aaabcac99f039e193
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-27 17:13:59 +0900

    home修正

--- FinalProject/HomeWindow/Home.swift
+++ FinalProject/HomeWindow/Home.swift
@@ -10,134 +10,78 @@ import SwiftUI
 
 struct Home: View {
     @ObservedObject var fetchPlans = FetchPlans()
-
+    
     
     @State var isDisabled = true
-
+    
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ZStack{
                 List(fetchPlans.plans, id: \.id) { plan in
-//                    NavigationLink (
-//                        destination: GroupUsers(planId: plan.plan_id)
-//                    ) {
-                        switch plan.state {
-                        case "募集中":
-                            NavigationLink {
-                            GroupUsers(planId: plan.plan_id)
-                            } label: {
-                                //                            print("募集中")
-                                HStack() {
-                                VStack(alignment: .leading, spacing: 10) {
-                                    
-                                    Text(String(plan.address))
-                                        .font(.subheadline)
-                                    Text(stringToStringDate(stringDate: plan.date, format: "MM/dd　HH:mm"))
-                                        .font(.title)
-                                    
-
-                                }
-                                .padding(.trailing, 20)
-                                    
-                                    VStack(alignment: .leading, spacing: 8) {
-                                        Text(String("\(plan.state)..."))
-                                            .font(.subheadline)
-                                        Text(String("\(plan.users_count) / 4人"))
-                                    }
-                                }
-                            }
-                            .padding()
-                        case "確定":
-                            NavigationLink {
-                            GroupUsers(planId: plan.plan_id)
-                            } label: {
-                                //                            print("募集中")
-                                HStack() {
-                                VStack(alignment: .leading, spacing: 10) {
-                                    
-                                    Text(String(plan.address))
-                                        .font(.subheadline)
-                                    Text(stringToStringDate(stringDate: plan.date, format: "MM/dd　HH:mm"))
-                                        .font(.title)
-                                    
-
-                                }
-                                .padding(.trailing, 20)
-                                    
-                                    VStack(alignment: .leading, spacing: 8) {
-                                        Text(String("\(plan.state)..."))
-                                            .font(.subheadline)
-                                        Text(String("\(plan.users_count) / 4人"))
-                                    }
-                                }
-                            }
-                            .padding()
-//                            print("確定")
-                                  case "終了":
-                            NavigationLink {
-                            GroupUsers(planId: plan.plan_id)
-                            } label: {
-                                //                            print("募集中")
-                                HStack() {
-                                VStack(alignment: .leading, spacing: 10) {
-                                    
-                                    Text(String(plan.address))
-                                        .font(.subheadline)
-                                    Text(stringToStringDate(stringDate: plan.date, format: "MM/dd　HH:mm"))
-                                        .font(.title)
-                                    
-
-                                }
-                                .padding(.trailing, 20)
-                                    
-                                    VStack(alignment: .leading, spacing: 8) {
-                                        Text(String("\(plan.state)..."))
-                                            .font(.subheadline)
-                                        Text(String("\(plan.users_count) / 4人"))
-                                    }
-                                }
-                            }
-                            .padding()
-                            .foregroundColor(.gray)
-//                                    print("終了")
-                                  default:
-                            NavigationLink (
-                                destination: GroupUsers(planId: plan.plan_id)
-                            ) {
-                            VStack(alignment: .leading, spacing: 8) {
+                    
+                    NavigationLink {
+                        GroupUsers(planId: plan.plan_id)
+                    } label: {
+                        HStack() {
+                            VStack(alignment: .leading, spacing: 0) {
                                 
                                 Text(String(plan.address))
-                                    .font(.headline)
+                                    .font(.subheadline)
                                 Text(stringToStringDate(stringDate: plan.date, format: "MM/dd　HH:mm"))
-                                    .font(.subheadline)
-                                Text(String(plan.state))
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
+                                    .font(.title)
                             }
-//                                    print("それ以外です")
+                            .padding(.trailing, 20)
+                            
+                            VStack(alignment: .leading, spacing: 8) {
+                                switch plan.state {
+                                case "募集中":
+                                    Text("\(plan.state)...")
+                                        .font(.subheadline)
+//                                        .foregroundColor(Color.customMainColor)
+                                case "確定":
+                                    Text("\(plan.state)")
+                                        .font(.subheadline)
+                                case "終了":
+                                    Text("\(plan.state)...")
+                                        .font(.subheadline)
+                                default:
+                                    Text("\(plan.state)")
+                                        .font(.subheadline)
+                                }
+                                Text("\(plan.users_count) / 4人")
+                            }
                         }
-
+                        .padding()
                     }
+                    .navigationTitle("HOME")
+                    .foregroundColor(plan.state == "終了" ? Color.customDarkGray : Color.customTextColor)
+                    .padding(.all, 5)
+                    .background(Color.white.opacity(0.3))
+                    .background(plan.state == "終了" ? Color.customDarkGray : Color.customlightGray)
+                    .cornerRadius(10)
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(Color.customlightGray)
+                    .shadow(color: .gray.opacity(0.7), radius: 3, x: 2, y: 2)
                 }
-                .navigationTitle("予定List")
+                .listStyle(.plain)
+                .background(Color.customlightGray)
+                
             }
-            
         }
     }
 }
 
+
 func stringToStringDate(stringDate: String, format:String) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.calendar = Calendar(identifier: .gregorian)
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ" //変換元のStringのDateの型に合わせる必要あり
-        let newDate =  dateFormatter.date(from: stringDate)!
-        dateFormatter.dateFormat = format
-        dateFormatter.locale = Locale(identifier: "ja_JP")//日本のタイムゾーン設定をする
-        let getDate = dateFormatter.string(from: newDate)
-        return getDate
-    }
-
-
+    let dateFormatter = DateFormatter()
+    dateFormatter.calendar = Calendar(identifier: .gregorian)
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ" //変換元のStringのDateの型に合わせる必要あり
+    let newDate =  dateFormatter.date(from: stringDate)!
+    dateFormatter.dateFormat = format
+    dateFormatter.locale = Locale(identifier: "ja_JP")//日本のタイムゾーン設定をする
+    let getDate = dateFormatter.string(from: newDate)
+    return getDate
+}
 
 
 #Preview {

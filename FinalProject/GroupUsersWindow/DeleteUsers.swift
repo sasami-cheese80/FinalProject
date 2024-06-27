2d4606d9947384b8732bfbb76f171acac5aec66c
kazuma_okada <kazuma_okada@mail.toyota.co.jp>
2024-06-27 13:07:38 +0900

    HOME画面の色編集

---
+++ FinalProject/GroupUsersWindow/DeleteUsers.swift
@@ -0,0 +1,8 @@
+//
+//  DeleteUsers.swift
+//  FinalProject
+//
+//  Created by user on 2024/06/27.
+//
+
+import Foundation
--- FinalProject/GroupUsersWindow/GroupUsers.swift
+++ FinalProject/GroupUsersWindow/GroupUsers.swift
@@ -24,32 +24,47 @@ struct GroupUsers: View {
     
     var body: some View {
         NavigationView {
-                    List {
-                        ForEach(fetchUsers.users, id: \.id) { user in
-                            VStack(alignment: .leading) {
-                                                    Text(user.name)
-                                                        .font(.headline)
-                                                    Text("ニックネーム: \(user.nickname)")
-                                                    Text("性別: \(user.gender)")
-                                                    Text("部署: \(user.department)")
-                                                }
-                                                .padding()
+            ZStack {
+                
+                List {
+                    ForEach(fetchUsers.users, id: \.id) { user in
+                        VStack(alignment: .leading) {
+                            Text(user.name)
+                                .font(.headline)
+                            Text("ニックネーム: \(user.nickname)")
+                            Text("性別: \(user.gender)")
+                            Text("部署: \(user.department)")
                         }
+                        .padding()
                     }
-                    .navigationBarTitle(Text("相乗りメンバー"))
-                    .navigationBarItems(trailing: Button(action: {
-//                        DeleteTask()
-                    })
-                                        {
-                        Text("グループから抜ける")
-                    }
-                    )
-        
                 }
+                .navigationBarTitle(Text("相乗りメンバー"))
+                //                    .navigationBarItems(trailing: Button(action: {
+                ////                        DeleteTask()
+                //                    })
+                //                                        {
+                //                        Text("グループから抜ける")
+                //                    }
+                //                    )
+                
+            }
+        }
+        Button(action: {
+            print("ここでpostします")
+//                        tabSelection = 3
+        }, label: {
+            Text("グループから抜ける")
+                .foregroundColor(.black)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .padding(.init(top: 15, leading: 70, bottom: 15, trailing: 70))
+                .background(Color(red: 1.0, green: 0.96, blue: 0.93))
+                .cornerRadius(10)
+                .border(.black, width: 1)
+        })
         Text("")
     }
 }
 
-//#Preview {
-//    GroupUsers(planId: 1)
-//}
+#Preview {
+    GroupUsers(planId: 1)
+}

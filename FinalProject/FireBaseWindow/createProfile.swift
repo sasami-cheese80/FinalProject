2eac4aed078b263b1ffd23a80d1016efb7ab271d
Yuta Sasaki <yuta.mybusiness@gmail.com>
2024-06-27 15:35:24 +0900

    プロフィール設定画面作ったよー

---
+++ FinalProject/FireBaseWindow/createProfile.swift
@@ -0,0 +1,18 @@
+//
+//  createProfile.swift
+//  FinalProject
+//
+//  Created by Yuta Sasaki  on 2024/06/27.
+//
+
+import SwiftUI
+
+struct createProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    createProfile()
+}

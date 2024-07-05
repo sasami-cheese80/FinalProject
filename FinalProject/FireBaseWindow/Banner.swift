14dbdc8746ae678c9cafe7e8a7c242280125c38e
yamada <yamada@dig>
2024-07-05 13:49:36 +0900

    新規登録時のバナー表示

---
+++ FinalProject/FireBaseWindow/Banner.swift
@@ -0,0 +1,18 @@
+//
+//  Banner.swift
+//  FinalProject
+//
+//  Created by yamada on 2024/07/05.
+//
+
+import SwiftUI
+
+struct Banner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Banner()
+}
